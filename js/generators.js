76c44b4924b472e0d26907d35fcc5561f467e5f9
Emma Goodliffe <32788926+EmmaGoodliffe@users.noreply.github.com>
2018-07-08 14:43:41 +0100

    updated generator.js

--- js/generators.js
+++ js/generators.js
@@ -1,18 +1,15 @@
-function fire(gen, max = 10) {
-  // get the generator read
-  let mine = gen();
-  // get a yielded value
-  let yielded = mine.next();
-  for (let i = 0; i <= max; i++) {
-    // if uncomplete go to the next yield with what was previously yielded
-    if (yielded.done === false) {
-      // console.log("y:", yielded, "i:", i, yielded.value === i);
-      yielded = mine.next(yielded.value);
-    } else {
-      //  otherwise say we're done
-      console.log("Done:", yielded.done);
-      break;
+function smarts (theGen, max = 8) {
+	const gen = theGen();
+  let got = gen.next();
+  let n = 0;
+  while (!got.done) {
+  	
+    got = gen.next(got.value);
+  	if (n > max) {
+    console.log("The emergency breaks were engaged.");
+    break;
     }
-  };
-  console.log("Your generator was fired");
+    n++;
+  }
+  
 }
