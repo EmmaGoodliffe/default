cf68d9e83215b16e4da2963f7f41c77ed1d8275b
Emma Goodliffe <32788926+EmmaGoodliffe@users.noreply.github.com>
2019-10-06 16:42:08 +0100

    Update server.js

--- server.js
+++ server.js
@@ -3,18 +3,19 @@ const fs = require('fs');
 const express = require('express');
 const app = express();
 
-app.get('/', function (req, res) {
-  res.sendFile(__dirname+"/index.html");
-});
-
+// For static files
 // app.use(express.static('src'));
+
+// Body parser for push requests
 app.use(express.urlencoded({ extended: false }));
 
-app.get('*', function (req, res) {
-  res.end("Page not found.");
+// Send index.html on '/' request
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + "/index.html");
 });
 
+// Use server on port
 const port = 3000;
-app.listen(port, function () {
+app.listen(port, () => {
   console.log("Listening at http://localhost:" + port);
 });
