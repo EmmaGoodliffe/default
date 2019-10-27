31af0193b0b5b93d768590671ccc519841354c4f
Emma Goodliffe <32788926+EmmaGoodliffe@users.noreply.github.com>
2019-10-27 12:34:51 +0000

    Update server.js with 404 detection

--- server.js
+++ server.js
@@ -14,6 +14,13 @@ app.get('/', (req, res) => {
   res.sendFile(__dirname + '/dist/index.html');
 });
 
+// Detect 404 request
+app.get('*', (req, res) => {
+  const result = `${req.protocol}://${req.get('host')}${req.originalUrl}`;
+  res.status(404);
+  res.end(`Cannot GET ${result}`);
+});
+
 // Use server on port
 const port = 1234;
 app.listen(port, () => {
